01bdf125ff9afdabf0927d099da2e201202e6e8c
Shoshana Swell <35551635+shoshanaswell@users.noreply.github.com>
2020-02-19 17:58:05 -0500

    Add files via upload

---
+++ code.py
@@ -0,0 +1,100 @@
+"""Welcome to a THIS IS MY SPACESUIT. workshop!
+This is the file you will use to code with your device.
+There is pseudocode to give hints to the code.
+After completing an example, you must comment out previous examples by using # on each line with code."""
+
+
+"""EXAMPLE 1: CHECK CHECK. This example prints "THIS IS MY SPACESUIT. to the serial. Use the two lines of code below"""
+#from adafruit_circuitplayground import cp
+print("THIS IS MY SPACESUIT.") #you can change the text to write whatever you want
+
+
+"""EXAMPLE 2: RAINBOW MAGIC. This example lights up the first NeoPixel. The rest of the example will have you light up the entire board!"""
+
+'''import statements - use 1 line of the code below'''
+#from adafruit_circuitplayground import cp
+
+
+#'''#sets the brightness of the LEDs- use the code below'''
+#cp.pixels.brightness = 0.1
+
+#to do: create a while loop that runs when it is true
+
+#to do: access the first LEDon the circle of your board - use the code below
+
+#to do: make more LEDs light up on the board by using the same notation as above. What will change? What color do you want to make?
+
+
+'''EXAMPLE 3: I HAVE THE WORLD IN MY FINGERPRINTS.
+This example lights up the entire board based on your touch'''
+
+'''import statements - use 1 line of code below'''
+#from adafruit_circuitplayground import cp
+
+
+
+'''sets the brightness of the LEDs- use the code below'''
+#cp.pixels.brightness = 0.1
+
+#to do: create a while loop that runs when it is true
+
+    # creates if statement if you touch the A1 port - use 1 line of code below
+    #if cp.touch_A1:
+        #to do: fill all pixels to one color
+
+    # to do: create an if statement if you touch the A2 port
+
+        #to do: fill all pixels to one color
+
+
+
+
+'''EXAMPLE 4: DJ IN THE HOUSE.
+This example plays a tone and shows an LED when you press a button'''
+
+'''import statements - use 1 lines of code below'''
+#from adafruit_circuitplayground import cp
+
+
+
+'''sets the brightness of the LEDs- use the code below'''
+#cp.pixels.brightness = 0.2
+
+#to do: create a while loop that runs when it is true
+
+    #to do: create an if statement for cp.button_a
+
+        #to do: access the 1st pixel like example 3 and set a color to it
+
+        #to do: use cp.start_tone() to tell the device how long to ring a tone. Insert number from 0-200
+
+    #to do: create an elif statement for cp.button_b
+
+        #to do: access the 2nd pixel like example 3 and set a color to it
+
+        #to do: use cp.start_tone() to tell the device how long to ring a tone. Insert number from 0-200
+
+    #to do: create an else statement
+
+        #to do: access the 1nd pixel like example 3 and set the color to (0,0,0)
+
+        #to do: access the 2nd pixel like example 3 and set the color to (0,0,0)
+
+        #stop the tone - use 1 line of the code below
+        #cp.stop_tone()
+
+
+'''EXAMPLE 5: DISSOLVE IN SPACE .
+This example should make the lights go on in a sequence. If you start with the first LED, it should stay on for a specific amount of time, then light up the next one. '''
+
+'''import statements - use 1 lines of code below'''
+#from adafruit_circuitplayground import cp
+#import time
+
+
+
+
+#contact info:
+# Instagram/Facebook: @thisismyspacesuit
+# Email: thisismyspacesuit@gmail.com
+# Website: thisismyspacesuit.com
