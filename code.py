51eb9131703c1143530ebd5d45ee0700a03915ce
Shoshana Swell <35551635+shoshanaswell@users.noreply.github.com>
2020-02-20 00:34:54 +0100

    Update code.py

--- code.py
+++ code.py
@@ -5,7 +5,7 @@ After completing an example, you must comment out previous examples by using # o
 
 
 """EXAMPLE 1: CHECK CHECK. This example prints "THIS IS MY SPACESUIT. to the serial. Use the two lines of code below"""
-#from adafruit_circuitplayground import cp
+from adafruit_circuitplayground import cp
 print("THIS IS MY SPACESUIT.") #you can change the text to write whatever you want
 
 
@@ -20,11 +20,10 @@ print("THIS IS MY SPACESUIT.") #you can change the text to write whatever you wa
 
 #to do: create a while loop that runs when it is true
 
-#to do: access the first LEDon the circle of your board - use the code below
-
+#to do: access the first LED on the circle of your board - use the code below
+    #cp.pixels[0] = (255,0,0)
 #to do: make more LEDs light up on the board by using the same notation as above. What will change? What color do you want to make?
-
-
+    
 '''EXAMPLE 3: I HAVE THE WORLD IN MY FINGERPRINTS.
 This example lights up the entire board based on your touch'''
 
@@ -41,11 +40,11 @@ This example lights up the entire board based on your touch'''
     # creates if statement if you touch the A1 port - use 1 line of code below
     #if cp.touch_A1:
         #to do: fill all pixels to one color
-
+        
     # to do: create an if statement if you touch the A2 port
-
+    
         #to do: fill all pixels to one color
-
+        
 
 
 
@@ -63,34 +62,25 @@ This example plays a tone and shows an LED when you press a button'''
 #to do: create a while loop that runs when it is true
 
     #to do: create an if statement for cp.button_a
-
+    
         #to do: access the 1st pixel like example 3 and set a color to it
-
+        
         #to do: use cp.start_tone() to tell the device how long to ring a tone. Insert number from 0-200
-
-    #to do: create an elif statement for cp.button_b
-
-        #to do: access the 2nd pixel like example 3 and set a color to it
-
-        #to do: use cp.start_tone() to tell the device how long to ring a tone. Insert number from 0-200
-
+        
     #to do: create an else statement
-
+    
         #to do: access the 1nd pixel like example 3 and set the color to (0,0,0)
-
-        #to do: access the 2nd pixel like example 3 and set the color to (0,0,0)
-
+        
         #stop the tone - use 1 line of the code below
-        #cp.stop_tone()
+        
 
 
 '''EXAMPLE 5: DISSOLVE IN SPACE .
 This example should make the lights go on in a sequence. If you start with the first LED, it should stay on for a specific amount of time, then light up the next one. '''
 
-'''import statements - use 1 lines of code below'''
+'''import statements - use 2 lines of code below'''
 #from adafruit_circuitplayground import cp
 #import time
-
 
 
 
